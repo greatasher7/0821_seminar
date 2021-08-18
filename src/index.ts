25526fc62b6a255c9e0e2eb3510d5541b88fed7a
Asher <greatasher7@gmail.com>
2021-08-18 14:17:53 +0900

    2days left

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import StackQueue from './stack-queue-ts/stack_queue';
+import Singleton from './design-pattern-ts/singleton';
+import Observer from './design-pattern-ts/observer';
+
+StackQueue();
+Singleton();
+Observer();
+
